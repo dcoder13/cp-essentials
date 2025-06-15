4add478aa943b45e115d997e2a90d9ef9356cd1d
dcoder13 <divyanshbisht2005@gmail.com>
2025-06-16 01:27:23 +0530

    code snippets backup

--- template.cpp
+++ template.cpp
@@ -1,4 +1,4 @@
-/*--------------DCODER13--------------*/
+/*---------------dcoder---------------*/
 
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
@@ -8,9 +8,9 @@ using namespace std;
 using namespace __gnu_pbds;
 
 #ifdef ON_PC
-    #include <debug.h>
+#include <debug.h>
 #else
-    #define debug(x...)
+#define debug(x...)
 #endif
 
 #define int long long int
@@ -22,38 +22,40 @@ using namespace __gnu_pbds;
 #define fi first
 #define se second
 #define all(v) v.begin(),v.end()
-#define BITS 32
-#define binary(n) bitset<BITS>(n).to_string()
-#define setbits(n) bitset<BITS>(n).count()
+#define sz(a) (int)a.size()
 
 template <typename T>
 using ord_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 //----------------------------------------------------------------------------------------------------------
 
-const int M=1e9 +7;
+const int M = 1e9 + 7;
 mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 
-void solve(){
+void solve() {
     int n;
-    cin>>n;
+    cin >> n;
     vector<int> a(n);
-    for(auto &x : a)cin>>x;
-    
+    for(auto &x : a)cin >> x;
+
 }
 
 //----------------------------------------------------------------------------------------------------------
 
-int32_t main(){
+int32_t main() {
+    auto begin = std::chrono::high_resolution_clock::now();
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int t=1;
-    //cin >> t;
-    while(t--){
+    int t = 1;
+    cin >> t;
+    for(int i = 0; i < t; i++) {
         // cout<<"Case #"<<i+1<<": ";
         solve();
-        cout<<endl;
+        cout << endl;
     }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
     return 0;
 }
